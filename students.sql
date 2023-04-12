0d4b88f06cc72dcfe571279bd4b91ba069eda28b
sjs53 <sjs53@uw.edu>
2023-04-12 12:16:19 -0700

    'What-a-Class-of-Students

---
+++ students.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE students (id INTEGER PRIMARY KEY NOT NULL, firstname varchar (40), lastname varchar (80),age INT);
+
+INSERT INTO students(firstname, lastname, age) VALUES ('Fred', 'Flintstone', 35);
+INSERT INTO students(firstname, lastname, age) VALUES ('Wilma', 'Flintstone', 29);
+INSERT INTO students(firstname, lastname, age) VALUES ('Barney', 'Rubble', 33);
+INSERT INTO students(firstname, lastname, age) VALUES ('Betty', 'Rubble', 29);
+INSERT INTO students(firstname, lastname, age) VALUES ('Pebbles', 'Flintstone', 1);
+INSERT INTO students(firstname, lastname, age) VALUES ('Bam-Bam', 'Rubble', 1);
+
+INSERT INTO students(firstname, lastname, age) VALUES ('Lucifer', 'Morningstar', 13700000000);
+INSERT INTO students(firstname, lastname, age) VALUES ('Trixie', 'Espinoza', 13);
+INSERT INTO students(firstname, lastname, age) VALUES ('Ella', 'Lopez', 33);
+INSERT INTO students(firstname, lastname, age) VALUES ('God', 'Johnson', 41);
