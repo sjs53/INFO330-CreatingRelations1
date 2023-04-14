6a75647cded25c4aca04d312f0030fea5633f22b
sjs53 <sjs53@uw.edu>
2023-04-13 21:43:32 -0700

    'added-data'

--- buildings.sql
+++ buildings.sql
@@ -5,13 +5,12 @@ CREATE TABLE rooms (number INT, buildingid INT, seating INT, FOREIGN KEY (buildi
 
 -- Code I used for Testing
 	--Adding Buildings
--- INSERT INTO buildings (name, shortname) VALUES ('Savery Hall', 'SAV');
--- INSERT INTO buildings (name, shortname) VALUES ('Odegaard Library', 'ODE');
--- INSERT INTO buildings (name, shortname) VALUES ('Denny', 'DEN');
--- INSERT INTO buildings (name, shortname) VALUES ('Mary Gates Hall', 'MGH');
+INSERT INTO buildings (name, shortname) VALUES ('Savery Hall', 'SAV');
+INSERT INTO buildings (name, shortname) VALUES ('Odegaard Library', 'ODE');
+INSERT INTO buildings (name, shortname) VALUES ('Denny', 'DEN');
+INSERT INTO buildings (name, shortname) VALUES ('Mary Gates Hall', 'MGH');
 
 	--Adding Rooms
---INSERT INTO rooms(number, buildingid, seating) VALUES (400, (SELECT id FROM buildings WHERE name = 
---'Mary Gates Hall'), 40);
---INSERT INTO rooms (number, buildingid, seating) VALUES (230, (SELECT id FROM buildings WHERE name = 
---'Odegaard Library'), 6);
+INSERT INTO rooms(number, buildingid, seating) VALUES (400, (SELECT id FROM buildings WHERE name = 'Mary Gates Hall'), 40);
+
+INSERT INTO rooms (number, buildingid, seating) VALUES (230, (SELECT id FROM buildings WHERE name = 'Odegaard Library'), 6);
